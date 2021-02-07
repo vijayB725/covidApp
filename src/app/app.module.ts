0e57c6123512a018742a379b475e70fbb9132e77
vijayB725 <kjvj725@gmail.com>
2021-02-07 12:38:43 +0530

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { CountriesComponent } from "./components/countries/countries.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
